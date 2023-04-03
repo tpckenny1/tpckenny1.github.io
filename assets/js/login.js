f70f98894f99c234d067622ee1ce84800a2cc86e
tpckenny1 <119289210+tpckenny1@users.noreply.github.com>
2023-04-03 11:42:30 +0000

    new accesskey function

---
+++ assets/js/login.js
@@ -0,0 +1,50 @@
+const btnLogin = document.getElementById("btn-login")
+const inputSchool = document.getElementById("input-school")
+const inputAccesskey = document.getElementById("input-accesskey")
+
+btnSet.addEventListener("click", () => {
+    setWithExpiry("accesskey", 
+        {
+            "school": inputSchool.value,
+            "accesskey": inputAccesskey.value
+        }, 
+        5000)
+})
+
+btnGet.addEventListener("click", () => {
+    const value = getWithExpiry("accesskey")
+    console.log(value)
+})
+
+function setWithExpiry(key, value, ttl) {
+    const now = new Date()
+
+    // `item` is an object which contains the original value
+    // as well as the time when it's supposed to expire
+    const item = {
+        value: value,
+        expiry: now.getTime() + ttl,
+    }
+    localStorage.setItem(key, JSON.stringify(item))
+}
+
+function getWithExpiry(key) {
+    const itemStr = localStorage.getItem(key)
+
+    // if the item doesn't exist, return null
+    if (!itemStr) {
+        return null
+    }
+
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+
+    // compare the expiry time of the item with the current time
+    if (now.getTime() > item.expiry) {
+        // If the item is expired, delete the item from storage
+        // and return null
+        localStorage.removeItem(key)
+        return null
+    }
+    return item.value
+}
